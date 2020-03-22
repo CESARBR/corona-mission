9d6ed25d30155b9739bed97f20defaab6cd00200
Vitor Casadei <vitor.casadei@gmail.com>
2020-03-22 11:59:22 -0300

    removing dob

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -10,7 +10,6 @@ import { NavController } from '@ionic/angular';
 })
 export class SignupPage implements OnInit {
   ionicForm: FormGroup;
-  defaultDate = "1992-06-30";
   isSubmitted = false;
 
   constructor(public formBuilder: FormBuilder, private navCtrl: NavController) { }
@@ -20,7 +19,6 @@ export class SignupPage implements OnInit {
       password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(30)]],
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       name: ['', [Validators.required, Validators.minLength(6)]],
-      dob: [this.defaultDate],
       passwordConfirmation: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(30)]]
     }, { 
       validators: this.password.bind(this)
@@ -31,13 +29,6 @@ export class SignupPage implements OnInit {
     const { value: password } = formGroup.get('password');
     const { value: passwordConfirmation } = formGroup.get('passwordConfirmation');
     return password === passwordConfirmation ? null : { passwordNotMatch: true };
-  }
-
-  getDate(e) {
-    let date = new Date(e.target.value).toISOString().substring(0, 10);
-    this.ionicForm.get('dob').setValue(date, {
-      onlyself: true
-    })
   }
   
   get errorControl() {
