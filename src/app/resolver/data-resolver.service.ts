340902166edb8f7e908ed35695c146acf0f38e38
Vitor Casadei <vitor.casadei@gmail.com>
2020-03-23 13:56:10 -0300

    Adding new pages and merge

---
+++ src/app/resolver/data-resolver.service.ts
@@ -0,0 +1,16 @@
+import { DataService } from './../services/data.service';
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class DataResolverService implements Resolve<any> {
+ 
+  constructor(private dataService: DataService) { }
+ 
+  resolve(route: ActivatedRouteSnapshot) {
+    let id = route.paramMap.get('id');
+    return id;
+  }
+}
