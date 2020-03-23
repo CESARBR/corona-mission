d7b4bde32e360cc9b41ef51bc857b0c32083cee7
Vitor Casadei <vitor.casadei@gmail.com>
2020-03-23 19:21:00 -0300

    Update

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { NavController, Platform } from '@ionic/angular';
 import { StorageService, Person } from '../../services/storage.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-register',
@@ -15,8 +16,10 @@ export class RegisterPage implements OnInit {
   persons: Person[] = [];
   newPerson: Person = <Person>{};  
 
+  people: any
+
   constructor(public formBuilder: FormBuilder, private navCtrl: NavController,
-    private storageService: StorageService, private plt:Platform) { 
+    private storageService: StorageService, private plt:Platform, private storage: Storage) { 
     this.plt.ready().then(() => {        
       this.loadPersons();
       this.storageService.deleteAll();
@@ -24,6 +27,15 @@ export class RegisterPage implements OnInit {
 }
 
   ngOnInit() {
+    // set a key/value
+    // this.storage.set('name', 'Max');
+
+    // Or to get a key/value pair
+    this.storage.get('persons').then((val) => {
+      console.log('persons is', val);
+      this.people = val;
+    });
+
     this.ionicForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       age: ['', [Validators.required]],
@@ -43,6 +55,24 @@ export class RegisterPage implements OnInit {
       console.log('Please provide all the required values!')
       return false;
     } else {
+      let newPerson = {
+        id: Date.now(),
+        name: this.ionicForm.value.name,
+        age: this.ionicForm.value.age,
+        phone: this.ionicForm.value.phone,
+        relationship: this.ionicForm.value.relationship,
+        mission: "Realizar missões!",
+        mission_color: "dark",
+        mission_label_color: "dark",
+        avatar: "../../assets/img/person_icon.png"
+      }
+      if (this.people === null) {
+        this.people = [newPerson]
+      } else {
+        this.people.push(newPerson)
+      }
+      this.storage.set('persons', this.people);
+      this.ionicForm.reset()
       this.navCtrl.setDirection('forward');
       this.navCtrl.navigateForward('/home');
     }
