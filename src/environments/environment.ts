13d0f457bd0591c651764408f7dc2d4b5337f2b2
gutaolox <32674347+gutaolox@users.noreply.github.com>
2020-03-23 17:36:11 -0300

    Setting up database and saving user

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD_ptmDUNmoBZRWwIGWB3yOm0sRRZXZ-sI",
+    authDomain: "coronamission-dev.firebaseapp.com",
+    databaseURL: "https://coronamission-dev.firebaseio.com",
+    projectId: "coronamission-dev",
+    storageBucket: "coronamission-dev.appspot.com",
+    messagingSenderId: "874703078872",
+    appId: "1:874703078872:web:f9ed58a591d66eea371ead",
+    measurementId: "G-87WR47M41P"
+  }
 };
 
 /*
