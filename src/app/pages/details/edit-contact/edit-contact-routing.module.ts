95aad07a33098a4737e59df5511f27c126042181
Jose Claudio <jcsj2@cesar.org.br>
2020-03-25 11:39:03 -0300

    Add resolve for edit route

--- src/app/pages/details/edit-contact/edit-contact-routing.module.ts
+++ src/app/pages/details/edit-contact/edit-contact-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { EditContactPage } from './edit-contact.page';
+import { DataResolverService } from 'src/app/resolver/data-resolver.service';
 
 const routes: Routes = [
   {
     path: '',
+    resolve: {
+      idContact: DataResolverService
+    },
     component: EditContactPage,
   },
   
