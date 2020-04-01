97db27329fdade993f95fd8a0a1b54255d511b09
Jose Claudio <jcsj2@cesar.org.br>
2020-04-01 10:02:50 -0300

    [logout_fix] - Fixing import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { AuthFirebaseService } from './Firebase-Services/firebase.Auth';
+import { AuthFirebaseService } from './services/firebase/firebase-auth.service';
 
 @Component({
   selector: 'app-root',
