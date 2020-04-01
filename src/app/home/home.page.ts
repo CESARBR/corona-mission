13c386b47f9b32667eccc3451b6b6720866bf3f3
gutaolox <32674347+gutaolox@users.noreply.github.com>
2020-04-01 13:56:22 -0300

    Making Tracking of how well are missions
    Makes a control of how much time the last mission was done and gives a feedback in color and text of the user is going good or not with that contact

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,9 @@ import { FirebaseDatabaseServices } from '../services/firebase/firebase-database
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  private readonly STATUS_UNCHECK = "ellipse-outline";
+  private readonly STATUS_CHECK = "checkmark-outline";
+
   hasRegistered = true;
 
   registeredUsers;
@@ -52,15 +55,15 @@ export class HomePage implements OnInit {
             contactValue.mission_color = "dark";
           }
           else {
-            contactValue.mission = daysAgoMissions.toString() + "Dias sem missão.";
-            contactValue.mission_label_color = "red";
-            contactValue.mission_color = "red";
+            contactValue.mission = daysAgoMissions.toString() + " Dias sem missão.";
+            contactValue.mission_label_color = "danger";
+            contactValue.mission_color = "danger";
           }
         }
         else {
           contactValue.mission = "Você fez " + missionsToday.toString() + " missões hoje.";
-          contactValue.mission_label_color = "dark";
-          contactValue.mission_color = "dark";
+          contactValue.mission_label_color = "secondary";
+          contactValue.mission_color = "secondary";
         }
       }
       else {
@@ -90,6 +93,7 @@ export class HomePage implements OnInit {
   getMissionsToday(challenges) {
     const date = new Date();
     return challenges.filter(challenge => challenge.lastChange &&
+      challenge.status === this.STATUS_CHECK &&
       (new Date(challenge.lastChange).getDate() === date.getDate() &&
         new Date(challenge.lastChange).getMonth() === date.getMonth() &&
         new Date(challenge.lastChange).getFullYear() === date.getFullYear())).length;
